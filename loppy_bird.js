599d7d1b3321102e63dcc2704826650215545b8c
luis69fernando <32930833+luis69fernando@users.noreply.github.com>
2017-11-20 20:07:17 -0200

    Add files via upload

--- loppy_bird.js
+++ loppy_bird.js
@@ -3,14 +3,30 @@ function setup(){
 	createCanvas(640, 480);
 	
 }
-if (KeyIsDown(32)){
-			start++;
-		}
+
 function draw(){
 	background(0);
+	if(keyIsDown(32)){
+		start=1;
+	}
+	if(start==0){
+		textSize(16);
+		fill(255);
+		text("aperte barra de espaço", 225, 240);
+	}	
+	
 		textSize(14);
 		fill(255);
 		text(pontos+" pontos", 280, 90);
+	
+	if(start==1){
+		if(obstX<posX&&obstX+48>posX&&obstHeight>=posY||obstX<=posX&&obstX+48>posX&&obstHeight+120<posY||obstX_2<posX&&obstX_2+48>posX&&obstHeight_2>=posY||obstX_2<=posX&&obstX_2+48>posX&&obstHeight_2+120<posY){
+		start=0;
+		obstX=640;
+		obstX_2=1000;
+		pontos=0;
+		posy=200
+	}
 		obstX-=3// velocidade do obstaculo
 		obstX_2-=3
 		if (keyIsDown(32)&&posY>0){
@@ -19,6 +35,8 @@ function draw(){
 		else if(posY<480){
 		posY+=2.5;//gravidade
 	}
+	
+}
 		ellipse(posX, posY, 25, 25);//passaro
 		if(obstX<-50){
 			obstX=640;//reiniciar obstaculo
@@ -36,4 +54,5 @@ function draw(){
 			pontos++;
 		}
 	}
+
 
